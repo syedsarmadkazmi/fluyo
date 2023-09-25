7ddf645678a542ada7b6bfb627c44a69d39e623c
Sarmad Kazmi <sarmadkazmi@Sarmad-Kazmi-Personal-MacBook.local>
2023-09-25 19:56:25 +0500

    fixed bg color for finish screen

--- src/screens/Finish/Finish.tsx
+++ src/screens/Finish/Finish.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
-		backgroundColor: THEME.COLORS.secondaryBlue,
+		backgroundColor: THEME.COLORS.primaryBlue,
 		paddingHorizontal: 20,
 	},
 	text: {
